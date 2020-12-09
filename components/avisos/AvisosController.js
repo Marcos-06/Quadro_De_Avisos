4994f8fadd2e71592454071d21b30fee8f03e570
Marcos Basso <marcoseduardodesousabassobasso@gmail.com>
2020-12-09 17:31:48 -0300

    Cadrastar Avisos

--- components/avisos/AvisosController.js
+++ components/avisos/AvisosController.js
@@ -1,15 +1,28 @@
 const router = require('express').Router()
 
-router.get("/",(req,res)=>{
+const Avisos = require('./Avisos')
+
+router.get("/", (req, res) => {
   res.render('index')
 })
 
-router.get("/avisos",(req,res)=>{
+router.get("/avisos", (req, res) => {
   res.render('avisos')
 })
 
-router.get("/avisos/novo",(req,res)=>{
+router.get("/avisos/novo", (req, res) => {
   res.render('formulario_avisos')
+})
+
+//sincrona = ao mesmo tempo
+//assincrona = em tempos diferentes
+router.post("/avisos/novo", async (req, res) => {
+  const titulo = req.body.titulo
+  const data = req.body.data
+  const mensagem = req.body.mensagem
+
+  const msg = await Avisos.salvar({titulo, data, mensagem})
+  res.render('formulario_avisos',{msg})
 })
 
 module.exports = router
---
+++ components/avisos/avisos.js
@@ -0,0 +1,27 @@
+//Conexão Com o Banco De Dados
+const db = require('../../knexfile.js')
+/**
+ * Inserir um aviso no banco de dados
+ * @param {object} aviso O aviso deve estar no seguinte formato: {titulo: <string>, data<string>, mensagem<string>}
+ * @returns {object} Mensagem de sucesso ou de erro
+ */
+function salvar(aviso) {
+    //insert
+    //db.insert(<obj com os dados>).into(<'<nome da tabela>'>)
+    return  db.insert(aviso).into('avisos')
+        .then(id => {
+            return {
+                tipo: "sucesso",
+                corpo: "Dados Inseridos Com Sucesso! "
+            }
+        })
+        .catch(erro => {
+            return {
+                tipo: "erro",
+                corpo: "Erro: "+erro
+            }
+        })
+}//Fim Do Salvar
+
+module.exports = {salvar}
+
